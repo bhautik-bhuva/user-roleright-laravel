28243ce8113e79d04751905d3b1fc1219dd2eca8
Bhargav Bhuva <bhargav.bhuva@techaxion.com>
2025-05-08 11:00:30 +0530

    1.0.13

--- src/UserAccessServiceProvider.php
+++ src/UserAccessServiceProvider.php
@@ -7,16 +7,20 @@ use Illuminate\Support\ServiceProvider;
 class UserAccessServiceProvider extends ServiceProvider
 {
     public function boot()
-    {  
+    {
+        // Automatically run migrations when the package is installed
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/database/migrations' => database_path('migrations'),
+            ], 'migrations');
+        }
     }
     
     public function register()
     {
-        $this->publishes([
-            __DIR__.'/database/migrations' => database_path('migrations'),
-        ], 'migrations');
-        
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
         // Load the routes from the routes directory
         $this->loadRoutesFrom(__DIR__.'/routes/DynamicRoutes.php');
-   }
+    }
 }
