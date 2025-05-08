64085a597106395ad9fe6e56bf86828af2dd7a4b
Bhargav Bhuva <bhargav.bhuva@techaxion.com>
2025-05-08 10:32:05 +0530

    remove code temp

--- src/UserAccessServiceProvider.php
+++ src/UserAccessServiceProvider.php
@@ -8,11 +8,11 @@ class UserAccessServiceProvider extends ServiceProvider
 {
     public function boot()
     {  
-        $this->loadMigrationsFrom(dirname(__DIR__,1).'/database/migrations');
+        // $this->loadMigrationsFrom(dirname(__DIR__,1).'/database/migrations');
 
-        $this->publishes([
-            dirname(__DIR__,1).'/config/access.php' => config_path('access.php'),
-        ]);
+        // $this->publishes([
+        //     dirname(__DIR__,1).'/config/access.php' => config_path('access.php'),
+        // ]);
     }
 
     public function register()
@@ -20,6 +20,6 @@ class UserAccessServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(dirname(__DIR__,1).'/routes/DynamicRoutes.php');
         
         // bindings or helpers
-        $this->mergeConfigFrom(dirname(__DIR__,1).'/config/access.php', 'access');
+        // $this->mergeConfigFrom(dirname(__DIR__,1).'/config/access.php', 'access');
     }
 }
